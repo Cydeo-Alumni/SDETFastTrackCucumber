09a21f4d0018066d24d35231350e5d0d59c1b43f
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-25 21:24:16 +0300

    project created.

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -1,4 +1,19 @@
 package com.cydeo.runner;
 
+
+import io.cucumber.junit.*;
+import io.cucumber.plugin.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = true,
+        tags = ""
+)
 public class CukesRunner {
 }
--- src/test/java/com/cydeo/step_definitions/WebTableLoginSteps.java
+++ src/test/java/com/cydeo/step_definitions/WebTableLoginSteps.java
@@ -1,6 +1,32 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.en.*;
+
 public class WebTableLoginSteps {
 
-
+    @Given("User goes to web table page")
+    public void user_goes_to_web_table_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters username")
+    public void user_enters_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters password")
+    public void user_enters_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User click login button")
+    public void user_click_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User is on dashboard")
+    public void user_is_on_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
